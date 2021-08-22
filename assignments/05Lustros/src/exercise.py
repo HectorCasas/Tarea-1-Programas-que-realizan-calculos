c2062360e2537b12d838856da85d7ad238980243
HectorCasas <88863220+HectorCasas@users.noreply.github.com>
2021-08-22 16:35:12 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    born=int(input('Dame el año de nacimiento: '))
+    year=int(input('Dame el año actual: '))
+    lustros=(year-born)/5
     print("Los lustros que has vivido son:",lustros)
 
 
